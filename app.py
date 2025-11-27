67284073ea9d95fa8517b23ebfe11917eddda5ae
nlahurte-iut90 <noe.lahurte@edu.univ-fcomte.fr>
2025-11-27 15:22:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -28,13 +28,86 @@ def show_layout():
     return render_template('layout.html')
 
 
-@app.route('/Achats/show')
+@app.route('/Achat/show')
 def show_achat():
     mycursor = get_db().cursor()
     sql=''' SELECT * FROM ACHAT '''
     mycursor.execute(sql)
-    Achats = mycursor.fetchall()
-    return render_template('Tables/Achats.html', Achats=Achats)
+    achats = mycursor.fetchall()
+    return render_template('Tables/Achats.html', achats=achats)
+
+@app.route('/Achat/add', methods=["GET"])
+def add_achat():
+    print('''affichage du formulaire pour enregistrer un Achat''')
+    return render_template('Tables/Achat_add.html')
+
+
+
+@app.route('/Achat/add', methods=['POST'])
+def valid_add_achat():
+    print('''ajout de L'achat dans le tableau''')
+    id_client = request.form.get('id_client')
+    montant_total = request.form.get('montant_total')
+    poids_total = request.form.get('poids_total')
+    date_achat = request.form.get('date_achat')
+    message = 'id du client :' + id_client + ' - montant total :' + montant_total + ' - poids total :' + poids_total + "- date d'achat :" + date_achat
+    print(message)
+    mycursor = get_db().cursor()
+    tuple_param=(id_client, montant_total, poids_total, date_achat)
+    sql="INSERT INTO ACHAT(id_client, montant_total, poids_total, date_achat ) VALUES (%s, %s, %s, %s);"
+    mycursor.execute(sql,tuple_param)
+    get_db().commit()
+    return redirect('/Achat/show')
+
+
+
+@app.route('/Achat/edit', methods=["GET"])
+def edit_achat():
+    print('''affichage du formulaire pour modifier un achat''')
+    print(request.args)
+    print(request.args.get('id_achat'))
+    id_achat=request.args.get('id_achat')
+    mycursor = get_db().cursor()
+    sql=''' SELECT id_achat , id_client,  montant_total , poids_total ,date_achat
+    FROM ACHAT
+    WHERE id_achat=%s;'''
+    tuple_param=(id_achat,)
+    mycursor.execute(sql,tuple_param)
+    achat = mycursor.fetchone()
+    return render_template('Tables/Achat_edit.html', achat=achat)
+
+@app.route('/Achat/edit', methods=["POST"])
+def valid_edit_achat():
+    print('''modification de l'achat dans le tableau''')
+    id_achat=request.form.get('id_achat')
+    id_client = request.form.get('id_client')
+    montant_total = request.form.get('montant_total')
+    poids_total = request.form.get('poids_total')
+    date_achat = request.form.get('date_achat')
+    message = 'id de l\'achat:' + id_achat + 'id du client :' + id_client + ' - montant total :' + montant_total + ' - poids total :' + poids_total + "- date d'achat :" + date_achat
+    print(message)
+    mycursor = get_db().cursor()
+    tuple_param=(id_client, montant_total, poids_total, date_achat, id_achat)
+    sql="UPDATE ACHAT SET id_client = %s, montant_total = %s, poids_total = %s, date_achat = %s WHERE id_achat=%s;"
+    mycursor.execute(sql,tuple_param)
+    get_db().commit()
+    return redirect('/Achat/show')
+
+
+@app.route('/Achat/delete')
+def delete_achat():
+    print('''suppression d'un achat''')
+    id_achat=request.args.get('id_achat',0)
+    print(id_achat)
+    mycursor = get_db().cursor()
+    tuple_param=(id_achat,)
+    sql="DELETE FROM ACHAT WHERE id_achat=%s;"
+    mycursor.execute(sql,tuple_param)
+
+    get_db().commit()
+    print(request.args)
+    print(request.args.get('id_achat'))
+    return redirect('/Achat/show')
 
 app.route('/Achats-vetements/show')
 def show_achats_vetements():
@@ -79,4 +152,5 @@ def edit():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
