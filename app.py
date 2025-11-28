a7606eb7f3097d712d41f5afdfd386b7c99df9fc
nlahurte-iut90 <noe.lahurte@edu.univ-fcomte.fr>
2025-11-28 11:00:04 +0100

    Enhance Achat routes with client details and totals
    Refactor Achat routes to include client names and totals.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, flash
-
+from datetime import date
 app = Flask(__name__)
 app.secret_key = 'votre_cle_secrete_ici'
 
@@ -31,10 +31,25 @@ def show_layout():
 @app.route('/Achat/show')
 def show_achat():
     mycursor = get_db().cursor()
-    sql=''' SELECT * FROM ACHAT '''
+    sql=''' SELECT id_achat, client_id, CLIENT.nom, CLIENT.prenom, montant_total, poids_total, date_achat 
+    FROM ACHAT
+    JOIN CLIENT ON ACHAT.client_id = CLIENT.id_client
+    GROUP BY id_achat, client_id
+    ORDER BY client_id ASC
+     '''
     mycursor.execute(sql)
     achats = mycursor.fetchall()
-    return render_template('Tables/Achats.html', achats=achats)
+    sql = '''
+        SELECT client_id, SUM(montant_total) AS total_depense, CLIENT.nom, CLIENT.prenom
+        FROM ACHAT
+        JOIN CLIENT ON ACHAT.client_id = CLIENT.id_client
+        GROUP BY client_id
+        ORDER BY total_depense DESC
+        LIMIT 1;
+    '''
+    mycursor.execute(sql)
+    client = mycursor.fetchone()
+    return render_template('Tables/Achats.html', achats=achats, client=client)
 
 @app.route('/Achat/add', methods=["GET"])
 def add_achat():
@@ -51,15 +66,17 @@ def add_achat():
 @app.route('/Achat/add', methods=['POST'])
 def valid_add_achat():
     print('''ajout de L'achat dans le tableau''')
-    id_client = request.form.get('id_client')
+    client_id = request.form.get('client_id')
     montant_total = request.form.get('montant_total')
     poids_total = request.form.get('poids_total')
     date_achat = request.form.get('date_achat')
-    message = 'id du client :' + id_client + ' - montant total :' + montant_total + ' - poids total :' + poids_total + "- date d'achat :" + date_achat
+    if not date_achat:
+        date_achat = date.today().strftime("%Y-%m-%d")
+    message = 'id du client :' + client_id + ' - montant total :' + montant_total + ' - poids total :' + poids_total + "- date d'achat :" + date_achat
     print(message)
     mycursor = get_db().cursor()
-    tuple_param=(id_client, montant_total, poids_total, date_achat)
-    sql="INSERT INTO ACHAT(id_client, montant_total, poids_total, date_achat ) VALUES (%s, %s, %s, %s);"
+    tuple_param=(client_id, montant_total, poids_total, date_achat)
+    sql="INSERT INTO ACHAT(client_id, montant_total, poids_total, date_achat ) VALUES (%s, %s, %s, %s);"
     mycursor.execute(sql,tuple_param)
     get_db().commit()
     return redirect('/Achat/show')
@@ -73,13 +90,13 @@ def edit_achat():
     print(request.args.get('id_achat'))
     id_achat=request.args.get('id_achat')
     mycursor = get_db().cursor()
-    sql=''' SELECT id_achat , id_client,  montant_total , poids_total ,date_achat 
+    sql=''' SELECT id_achat , client_id,  montant_total , poids_total ,date_achat 
     FROM ACHAT
     WHERE id_achat=%s;'''
     tuple_param=(id_achat,)
     mycursor.execute(sql,tuple_param)
     achat = mycursor.fetchone()
-    sql=''' SELECT id_client, nom, prenom
+    sql=''' SELECT client_id, nom, prenom
     FROM CLIENT;'''
     mycursor.execute(sql)
     clients = mycursor.fetchall()
@@ -89,15 +106,15 @@ def edit_achat():
 def valid_edit_achat():
     print('''modification de l'achat dans le tableau''')
     id_achat=request.form.get('id_achat')
-    id_client = request.form.get('id_client')
+    client_id = request.form.get('client_id')
     montant_total = request.form.get('montant_total')
     poids_total = request.form.get('poids_total')
     date_achat = request.form.get('date_achat')
-    message = 'id de l\'achat:' + id_achat + 'id du client :' + id_client + ' - montant total :' + montant_total + ' - poids total :' + poids_total + "- date d'achat :" + date_achat
+    message = 'id de l\'achat:' + id_achat + 'id du client :' + client_id + ' - montant total :' + montant_total + ' - poids total :' + poids_total + "- date d'achat :" + date_achat
     print(message)
     mycursor = get_db().cursor()
-    tuple_param=(id_client, montant_total, poids_total, date_achat, id_achat)
-    sql="UPDATE ACHAT SET id_client = %s, montant_total = %s, poids_total = %s, date_achat = %s WHERE id_achat=%s;"
+    tuple_param=(client_id, montant_total, poids_total, date_achat, id_achat)
+    sql="UPDATE ACHAT SET client_id = %s, montant_total = %s, poids_total = %s, date_achat = %s WHERE id_achat=%s;"
     mycursor.execute(sql,tuple_param)
     get_db().commit()
     return redirect('/Achat/show')
@@ -205,6 +222,7 @@ def valid_edit_collecte_vetements():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
